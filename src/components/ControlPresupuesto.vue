ffa8a4fa15289acb9976875ae0f6b4b4d8228db5
Dorian Gonzalez <doriandarren1@gmail.com>
2024-10-02 20:59:44 +0200

    finish project

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -4,11 +4,15 @@
 
         <div class="contenedor-grafico">
 
+            <p class="porcentaje">{{  porcentaje  }}%</p>
+
             <CircleProgress 
-                :percent="20"
+                :percent="porcentaje"
                 :size="250"
-                :border-width="50"
-                :border-bg-width="50"
+                :border-width="30"
+                :border-bg-width="30"
+                fill-color="#3b82f6"
+                empty-color="#e1e1e1"
             />
             
         </div>
@@ -42,6 +46,7 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import CircleProgress from 'vue3-circle-progress'
 import "vue3-circle-progress/dist/circle-progress.css"
 import { formatearCantidad } from '../helpers'
@@ -64,9 +69,31 @@ const props = defineProps({
     }
 })
 
+const porcentaje = computed(() => {
+    return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100)
+})
+
 </script>
 
 <style scoped>
+
+.contenedor-grafico{
+    position: relative;
+}
+
+.porcentaje{
+    position: absolute;
+    margin: auto;
+    top: calc(50% - 1.5rem);
+    left: 0;
+    right: 0;
+    text-align: center;
+    z-index: 100;
+    font-size: 3rem;
+    font-weight: 900;
+    color: var(--gris-oscuro);
+}
+
 
 .dos-columnas{
     display: flex;
